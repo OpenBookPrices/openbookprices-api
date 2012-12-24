a6542ba90043060ae4a878688a7913497913695d
Edmund von der Burg <evdb@mysociety.org>
2012-12-24 16:21:12 +0000

    Add handler to return faked book results

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ module.exports = function () {
   
   var app = express();
 
+  // fake handler for the books endpoints
+  app.get("/books/:isbn", function (req, res) {
+    var isbn = req.param("isbn");
+    res.jsonp({
+      isbn:   isbn,
+      title:  "Title of " + isbn,
+      author: "Author of " + isbn,
+    });
+  });
+
   // 404 everything
   app.all("*", function (req, res) {
     res.status(404);
