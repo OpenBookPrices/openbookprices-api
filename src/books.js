ec0f6d9815a7e8dad92162444712b7701a322a99
Edmund von der Burg <evdb@mysociety.org>
2013-03-06 22:05:31 +0000

    Add isbn validation and redirecting if it is not correct

--- src/books.js
+++ src/books.js
@@ -1,10 +1,34 @@
 "use strict";
 
-var express = require("express");
+var express = require("express"),
+    ean     = require("ean");
 
 var app = module.exports = express();
 
 app.use(app.router);
+
+app.param("isbn", function (req, res, next) {
+  
+  var dirty = req.param("isbn");
+  var clean = dirty.replace(/\D/g, "");
+
+  // isbn10 to isbn13 - see http://www.isbn-13.info/ for algorithm
+  if (clean.length == 10) {
+    clean = "978" + clean.substr(0, 9);
+    clean += ean.checksum(clean.split(""));
+  }
+  
+  if (!clean || !ean.isValid(clean)) {
+    return next("404");
+  } else if (clean != dirty) {
+    // FIXME - could break in interesting ways. Better to reconstruct the URL properly
+    return res.redirect(req.originalUrl.replace(dirty, clean));
+  }
+  
+  req.param("isbn", clean);
+  next();
+});
+
 
 // fake handler for the books endpoints
 app.get("/:isbn", function (req, res) {
@@ -18,10 +42,16 @@ app.get("/:isbn", function (req, res) {
 
 // fake handler for the books endpoints
 app.get("/:isbn/prices/:country/:currency", function (req, res) {
-  // var isbn = req.param("isbn");
+
+  // var isbn     = req.param("isbn");
+  // var country  = req.param("country");
+  // var currency = req.param("currency");
+  
   res.jsonp([
     { price: 56.78 },
     { price: 12.34 },
     { price: 34.56 },
   ]);
 });
+
+
