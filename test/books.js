0c53a872588976c5aa6838946df982c36b836f55
Edmund von der Burg <evdb@mysociety.org>
2013-03-13 20:51:51 +0000

    Test the api app, not the book app, so that we see the full paths being used

--- test/books.js
+++ test/books.js
@@ -2,32 +2,32 @@
 
 // var assert = require("assert");
 var request = require("supertest"),
-    booksApp = require("../src/books");
+    apiApp  = require("../");
 
-request = request(booksApp);
+request = request(apiApp());
 
 describe("Books", function () {
   describe("book details", function () {
 
     it("should redirect to normalised isbn13", function (done) {
       request
-        .get("/0340831499")
+        .get("/books/0340831499")
         .expect(302)
-        .expect("Location", "/9780340831496")
+        .expect("Location", "/books/9780340831496")
         .end(done);
     });
 
     it("should redirect to normalised isbn13 (callback)", function (done) {
       request
-        .get("/0340831499?callback=foobar")
+        .get("/books/0340831499?callback=foobar")
         .expect(302)
-        .expect("Location", "/9780340831496?callback=foobar")
+        .expect("Location", "/books/9780340831496?callback=foobar")
         .end(done);
     });
 
     it("should return 404 when the isbn is not valid", function (done) {
       request
-        .get("/123456789")
+        .get("/books/123456789")
         .expect(404)
         .expect("Content-Type", "application/json; charset=utf-8")
         .expect({ error: "isbn '123456789' is not valid" })
@@ -36,7 +36,7 @@ describe("Books", function () {
 
     it("should return 404 when the isbn is not valid (callback)", function (done) {
       request
-        .get("/123456789?callback=foobar")
+        .get("/books/123456789?callback=foobar")
         .expect(404)
         .expect("Content-Type", "text/javascript; charset=utf-8")
         .expect(/^foobar && foobar\(\{/)
@@ -45,7 +45,7 @@ describe("Books", function () {
 
     it("should return correct details for valid isbn", function (done) {
       request
-        .get("/9780340831496")
+        .get("/books/9780340831496")
         .expect(200)
         .expect("Content-Type", "application/json; charset=utf-8")
         .expect({
@@ -58,7 +58,7 @@ describe("Books", function () {
 
     it("should return correct details in callback", function (done) {
       request
-        .get("/9780340831496?callback=foobar")
+        .get("/books/9780340831496?callback=foobar")
         .expect(200)
         .expect("Content-Type", "text/javascript; charset=utf-8")
         .expect(/^foobar && foobar\(\{/)
