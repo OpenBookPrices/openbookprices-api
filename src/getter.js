3769ce9f09658a3748209193b6dc29e68c9160dd
Edmund von der Burg <evdb@mysociety.org>
2013-03-15 21:53:40 +0000

    Split up the book detail fetching into smaller chunks

--- src/getter.js
+++ src/getter.js
@@ -7,15 +7,29 @@ var
     Fetcher = require("l2b-price-fetchers");
 
 
-module.exports.getBookDetails = function (isbn, cb) {
-
-  var f = new Fetcher();
-  f.fetch(
+function getBookDetails (isbn, cb) {
+  fetchFromScrapers(
     {vendor: "foyles", isbn: isbn },
-    function (err, data) {
+    function (err, results) {
       if (err) { return cb(err); }
-      var returnData = _.pick(data, "isbn", "authors", "title");
+      var returnData = _.pick(results, "isbn", "authors", "title");
       cb(null, returnData);
     }
   );
+}
+
+
+function fetchFromScrapers (options, cb) {
+  var f = new Fetcher();
+  f.fetch(
+    options,
+    function (err, data) {
+      if (err) { return cb(err); }
+      cb(null, data);
+    }
+  );
+}
+
+module.exports = {
+  getBookDetails: getBookDetails,
 };
