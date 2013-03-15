a0bac69c812d2e629de8f4aef96fb43d3709de08
Edmund von der Burg <evdb@mysociety.org>
2013-03-15 21:59:29 +0000

    Cache the book details in redis

--- src/getter.js
+++ src/getter.js
@@ -1,23 +1,33 @@
 "use strict";
 
-var
-    // redis   = require("redis"),
-    // client  = redis.createClient(),
+var redis   = require("redis"),
+    client  = redis.createClient(),
     _       = require("underscore"),
     Fetcher = require("l2b-price-fetchers");
 
 
 function getBookDetails (isbn, cb) {
-  fetchFromScrapers(
-    {vendor: "foyles", isbn: isbn },
-    function (err, results) {
-      if (err) { return cb(err); }
-      var returnData = _.pick(results, "isbn", "authors", "title");
-      cb(null, returnData);
-    }
-  );
-}
 
+  var cacheKey = "details-" + isbn;
+
+  client.get(cacheKey, function (err, reply) {
+    if ( reply ) {
+      cb( null, JSON.parse(reply) );
+    } else {
+      fetchFromScrapers(
+        {vendor: "foyles", isbn: isbn },
+        function (err, results) {
+          if (err) { return cb(err); }
+          var returnData = _.pick(results, "isbn", "authors", "title");
+          // TODO perhaps put an expiry in here...
+          client.set(cacheKey, JSON.stringify(returnData));
+          cb(null, returnData);
+        }
+      );
+    }
+  });
+
+}
 
 function fetchFromScrapers (options, cb) {
   var f = new Fetcher();
@@ -29,6 +39,7 @@ function fetchFromScrapers (options, cb) {
     }
   );
 }
+
 
 module.exports = {
   getBookDetails: getBookDetails,
