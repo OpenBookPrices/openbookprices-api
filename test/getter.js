b1a0463a66e95c4ff7859b96f62473005b2f508d
Edmund von der Burg <evdb@mysociety.org>
2013-06-07 06:42:38 +0100

    Stub the actual scrape to speed up the tests

--- test/getter.js
+++ test/getter.js
@@ -1,18 +1,41 @@
 "use strict";
 
 var assert = require("assert"),
-    // sinon  = require("sinon"),
+    sinon  = require("sinon"),
+    fetcher = require("l2b-price-fetchers"),
     getter = require("../src/getter");
 
 describe("Getter", function () {
 
+  var sandbox;
+
   beforeEach(function (done) {
     getter.enterTestMode(done);
+    sandbox = sinon.sandbox.create();
+  });
+
+  afterEach(function () {
+    sandbox.restore();
   });
 
   it("should get book details, then cache", function (done) {
 
-
+    // stub the country so that GB is not accepted
+    sandbox.stub(fetcher, "fetch").yields(
+      null,
+      {
+        args: {
+          vendor: "foyles",
+          isbn: "9780340831496",
+          country: "GB",
+          currency: "GBP",
+        },
+        url: "http://www.foyles.co.uk/witem/food-drink/mcgee-on-food-and-cooking-an,harold-mcgee-9780340831496",
+        title: "McGee on Food and Cooking: An Encyclopedia of Kitchen Science, History and Culture",
+        authors: [ "Harold McGee" ],
+        prices: [],
+      }
+    );
 
     getter.getBookDetails("9780340831496", function (err, details) {
       assert.deepEqual(details, {
