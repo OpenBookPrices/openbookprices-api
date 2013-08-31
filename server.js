aab04c704a0c95c7cca8aabab16f8e9a57584433
Edmund von der Burg <evdb@mysociety.org>
2013-08-31 11:51:04 +0100

    jshint fix

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.errorHandler());
 app.use( apiApp() );
 
 // get the port from the commandline, or default
-var port = process.argv[2] || 3000
+var port = process.argv[2] || 3000;
 console.log("Listening on port %s", port);
 
 app.listen(port);
