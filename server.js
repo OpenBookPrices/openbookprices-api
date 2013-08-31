706bbb8ce628ce6679d1743bd652390f1cdab870
Edmund von der Burg <evdb@mysociety.org>
2013-08-31 07:45:34 +0100

    Simple server wrapper

---
+++ server.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var express = require("express"),
+    apiApp  = require("./index");
+
+var app = express();
+
+// Make dev easier
+app.use(express.logger("dev"));
+app.use(express.errorHandler());
+
+app.use( apiApp() );
+
+// get the port from the commandline, or default
+var port = process.argv[2] || 3000
+console.log("Listening on port %s", port);
+
+app.listen(port);
