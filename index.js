ec00a765bc8462b741222858c2c3059c21250e6c
Edmund von der Burg <evdb@mysociety.org>
2013-09-01 19:48:49 +0100

    Move as much of the server into the API app as possible

--- index.js
+++ index.js
@@ -7,28 +7,37 @@ module.exports = function () {
 
   var app = express();
 
-  // By default don't cache anything (much easier when working with CloudFront)
-  app.use( function (req, res, next) {
-    res.header( "Cache-Control", helpers.cacheControl(0) );
-    next();
-  });
+  // development only
+  if ("development" == app.get("env")) {
+    app.use(express.logger("dev"));
+  }
 
-  // Allow all domains to request data (see CORS for more details)
+  // Set up the default response
   app.use(function (req, res, next) {
-    res.set("Access-Control-Allow-Origin", "*");
+
+    // By default don't cache anything (much easier when working with CloudFront)
+    res.header( "Cache-Control", helpers.cacheControl(0) );
+
+    // Allow all domains to request data (see CORS for more details)
+    res.header("Access-Control-Allow-Origin", "*");
+
     next();
   });
 
+  // Load the sub-apps
   app.use("/country", require("./src/country"));
   app.use("/books",   require("./src/books"));
   app.use("/prices",  require("./src/prices"));
   app.use("/echo",    require("./src/echo"));
 
-  // 404 everything
+  // 404 everything that was not caught above
   app.all("*", function (req, res) {
     res.status(404);
     res.jsonp({ error: "404 - page not found" });
   });
+
+  // Default error handling - TODO change to JSON
+  app.use(express.errorHandler());
 
   return app;
 };
--- server.js
+++ server.js
@@ -1,18 +1,11 @@
 "use strict";
 
-var express = require("express"),
-    apiApp  = require("./index");
+var apiApp  = require("./index");
 
-var app = express();
-
-// Make dev easier
-app.use(express.logger("dev"));
-app.use(express.errorHandler());
-
-app.use( apiApp() );
+var app = apiApp();
 
 // get the port from the commandline, or default
 var port = process.argv[2] || 3000;
-console.log("Listening on port %s", port);
 
+console.log("Listening on port %s", port);
 app.listen(port);
