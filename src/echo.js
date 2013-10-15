d9784cb824711f758fa2f5f52403795ac86d8be9
Edmund von der Burg <evdb@mysociety.org>
2013-10-15 18:56:47 +0100

    Send less details back in the echo response (just the useful ones really)

--- src/echo.js
+++ src/echo.js
@@ -10,26 +10,12 @@ app.use(app.router);
 app.all(
   "*",
   function (req, res) {
-
-    // send it back as data
     res.jsonp({
       timestamp: Date.now()/1000,
-      request: {
-        method: req.method,
-        secure: req.secure,
-        protocol: req.protocol,
-        host: req.host,
-        subdomains: req.subdomains,
-        url: req.url,
-        originalUrl: req.originalUrl,
-        path: req.path,
-        query: req.query,
-      },
       network: {
         ip: req.ip,
         ips: req.ips,
       },
-      headers: req.headers,
     });
 
   }
