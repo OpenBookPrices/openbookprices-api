531e3caf38d4b4594e68eb968f2ae50892dc92f8
Edmund von der Burg <evdb@ecclestoad.co.uk>
2014-02-25 12:39:18 -0300

    Make it clear we are using a wrapped callback

--- src/getter.js
+++ src/getter.js
@@ -83,7 +83,7 @@ function getBookPricesForVendor (args, cb) {
   scrapeArgs.currency = fetcher.currencyForVendor(args.vendor, args.currency);
 
   // Wrap the callback so that we can tidy up the response
-  cb = _.wrap(cb, function (func, err, rawResult) {
+  var wrappedCB = _.wrap(cb, function (func, err, rawResult) {
 
     if (err) {
       // something went wrong with the scraper. Store an error in cache to back
@@ -148,23 +148,23 @@ function getBookPricesForVendor (args, cb) {
 
   client.get(cacheKey, function (err, reply) {
     if ( reply ) {
-      return cb( null, JSON.parse(reply) );
+      return wrappedCB( null, JSON.parse(reply) );
     } else if (args.fromCacheOnly) {
 
       var emptyResponse = _.omit(args, "fromCacheOnly");
       emptyResponse.ttl = 0;
       emptyResponse.timestamp = null;
       emptyResponse.url = null;
-      return cb( null, emptyResponse);
+      return wrappedCB( null, emptyResponse);
 
     } else {
       fetchFromScrapers(
         scrapeArgs,
         function (err, results) {
-          if (err) { return cb(err); }
+          if (err) { return wrappedCB(err); }
           var bookPrices = extractBookPrices(results);
           cacheBookPrices(bookPrices);
-          return cb(null, bookPrices[scrapeArgs.country]);
+          return wrappedCB(null, bookPrices[scrapeArgs.country]);
         }
       );
     }
